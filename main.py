2cc5d63c2534c58a217356fce71fb4e28d58adc4
Jerossas <mosqueralozanojhoanferley@gmail.com>
2026-07-29 20:02:49 -0500

    refactor: organize main code

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 import os
-from openai import OpenAI
+
 from dotenv import load_dotenv
-
-load_dotenv()
-
-api_key = os.environ.get("OPENROUTER_API_KEY")
-
-if api_key == None:
-    raise RuntimeError("The OpenRouter API key was not found.")
+from openai import OpenAI
 
 
-client: OpenAI = OpenAI(
-    base_url = "https://openrouter.ai/api/v1",
-    api_key = api_key
-)
+def main() -> None:
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if not api_key:
+        raise RuntimeError("OPENROUTER_API_KEY environment variable not set")
 
-messages: list[dict] = [
-        {
-            "role": "user",
-            "content": "Why is boot.dev such a great place to learn backend development? Use one paragraph maximum."
-        }
-]
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+            }
+        ],
+    )
 
-def main():
-    
-    response = client.chat.completions.create(model="openrouter/free", messages=messages)
+    print("Response:")
     print(response.choices[0].message.content)
 
 
