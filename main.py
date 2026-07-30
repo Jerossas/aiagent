fe4498a5b698fbf5bb34cea5bc28cbc6654dd0a3
Jerossas <mosqueralozanojhoanferley@gmail.com>
2026-07-29 20:37:23 -0500

    feat: add usage report along with the response message
    - added prompt token usage
    - added completion token usage

--- main.py
+++ main.py
@@ -14,15 +14,24 @@ def main() -> None:
         base_url="https://openrouter.ai/api/v1",
         api_key=api_key,
     )
+    user_prompt: str = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum." 
     response = client.chat.completions.create(
         model="openrouter/free",
         messages=[
             {
                 "role": "user",
-                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+                "content": user_prompt,
             }
         ],
     )
+
+    if response.usage == None:
+        raise RuntimeError("The API request failed. Usage field is empty.")
+
+    print(f"User prompt: {user_prompt}")
+
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
 
     print("Response:")
     print(response.choices[0].message.content)
