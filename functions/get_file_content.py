58780d572f7cb141ec4050113ecb5ade21f4f63b
Jerossas <mosqueralozanojhoanferley@gmail.com>
2026-07-30 18:58:11 -0500

    feat: list content of a file within strict limits
    - to handle carefully the token usage, reading from a file is limited to 10000 caracters

---
+++ functions/get_file_content.py
@@ -0,0 +1,29 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory: str, file_path: str):
+
+    try:
+
+        abs_working_dir = os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
+
+        if os.path.commonpath([abs_working_dir, target_path]) != abs_working_dir:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(target_path):
+            return f'Error: File not found or is not a regular file: {file_path}'
+
+        with open(target_path, "r") as file:
+            file_content: str = file.read(MAX_CHARS)
+
+            if file.read(1):
+                file_content += f"[...File \"{target_path}\" truncated at {MAX_CHARS} characters]"
+
+            return file_content
+
+    except Exception as e:
+        return f"Error: {e}"
+
+if __name__ == "__main__":
+    pass
