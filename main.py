43204c802c86d86711f8693cbf1318f1d71c75a0
Jerossas <mosqueralozanojhoanferley@gmail.com>
2026-07-29 19:54:49 -0500

    feat: first API call with harcode message

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.environ.get("OPENROUTER_API_KEY")
+
+if api_key == None:
+    raise RuntimeError("The OpenRouter API key was not found.")
+
+
+client: OpenAI = OpenAI(
+    base_url = "https://openrouter.ai/api/v1",
+    api_key = api_key
+)
+
+messages: list[dict] = [
+        {
+            "role": "user",
+            "content": "Why is boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        }
+]
+
 def main():
-    print("Hello from aiagent!")
+    
+    response = client.chat.completions.create(model="openrouter/free", messages=messages)
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
